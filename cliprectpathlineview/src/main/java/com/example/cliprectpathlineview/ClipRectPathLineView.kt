fc708abb2cc302a277d4c22134491a847848a855
Anwesh43 <anweshthecool0@gmail.com>
2021-04-02 20:54:40 +0530

    adding Canvas extension methods

--- cliprectpathlineview/src/main/java/com/example/cliprectpathlineview/ClipRectPathLineView.kt
+++ cliprectpathlineview/src/main/java/com/example/cliprectpathlineview/ClipRectPathLineView.kt
@@ -8,6 +8,8 @@ import android.graphics.Paint
 import android.graphics.RectF
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Path
+
 
 val colors : Array<Int> = arrayOf(
     "#f44336",
@@ -29,3 +31,46 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawClipRectPathLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        drawLine(-w / 2, 0f, -w / 2 + w * 0.25f * sf1, 0f, paint)
+        drawLine(-w / 4, 0f, -w / 4 + (w / 4) * sf2, -(h / 2) * sf2, paint)
+        save()
+        val path : Path = Path()
+        path.moveTo(-w / 2, 0f)
+        path.lineTo(-w / 4, 0f)
+        path.lineTo(0f, -h / 2)
+        path.lineTo(0f, 0f)
+        clipPath(path)
+        drawRect(
+            RectF(
+                -w / 2,
+                -h / 2,
+                -w / 2 + (w * 0.5f) * sf3,
+                0f
+            ),
+            paint
+        )
+        restore()
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawCRPLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawClipRectPathLine(scale, w, h, paint)
+}
