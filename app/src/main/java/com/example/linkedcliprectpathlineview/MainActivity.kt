a7234449c01b6ecd771d61b5a5d5cdd35cec618f
Anwesh43 <anweshthecool0@gmail.com>
2021-04-02 22:19:58 +0530

    adding view in activity

--- app/src/main/java/com/example/linkedcliprectpathlineview/MainActivity.kt
+++ app/src/main/java/com/example/linkedcliprectpathlineview/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.linkedcliprectpathlineview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.cliprectpathlineview.ClipRectPathLineView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ClipRectPathLineView.create(this)
+        fullScreen()
     }
+}
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
