add8749790e6b58a4391ff3182089b8db5395c1c
Anwesh43 <anweshthecool0@gmail.com>
2021-04-02 18:59:37 +0530

    adding constants and libraries

--- cliprectpathlineview/src/main/java/com/example/cliprectpathlineview/ClipRectPathLineView.kt
+++ cliprectpathlineview/src/main/java/com/example/cliprectpathlineview/ClipRectPathLineView.kt
@@ -1,2 +1,26 @@
 package com.example.cliprectpathlineview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.graphics.Color
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#673AB7",
+    "#F57F17",
+    "#0277BD",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 2.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
