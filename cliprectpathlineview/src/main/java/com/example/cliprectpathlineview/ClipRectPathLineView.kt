bcee6ca4fc90288bcba2618ea18735f94c68721a
Anwesh43 <anweshthecool0@gmail.com>
2021-04-02 21:25:53 +0530

    adding CRPLNode class

--- cliprectpathlineview/src/main/java/com/example/cliprectpathlineview/ClipRectPathLineView.kt
+++ cliprectpathlineview/src/main/java/com/example/cliprectpathlineview/ClipRectPathLineView.kt
@@ -139,4 +139,45 @@ class ClipRectPathLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CRPLNode(var i : Int, val state : State = State()) {
+
+        private var next : CRPLNode? = null
+        private var prev : CRPLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = CRPLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCRPLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CRPLNode {
+            var curr: CRPLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
